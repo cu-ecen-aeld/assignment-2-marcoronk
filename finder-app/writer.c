83dafd82c61b987e8bf552e91ba79917a76d4636
marcoronk <35136040+marcoronk@users.noreply.github.com>
2024-11-28 07:59:52 +0100

    Update writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -8,35 +8,32 @@
 #include <syslog.h>
 
 int main(int argc, char *argv[]) {
-     char buffer[1024];
-
+    char buffer[1024];
+    int ret=0;
     memset(buffer,0,1024);
 
     if (argc != 3) {
-        fprintf(stderr, "Error: Missing arguments.\n");
-        fprintf(stderr, "Usage: %s <file_path> <text_to_write>\n", argv[0]);
         return 1;
     }
 
     openlog("writer", LOG_PID | LOG_CONS, LOG_USER);
     const char *writefile = argv[1];
     const char *writestr = argv[2];
-
-
-    FILE *file = fopen(writefile, "a");
-    if (file == NULL) {
-        fprintf(stderr, "Error: Could not create or open the file %s.\n", writefile);
+    
+    int fd = open(writefile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (fd == -1) {
         syslog(LOG_ERR, "Error: Could not create or open the file %s.\n", writefile);
         return 1;
     }
-
-    // Write the content to the file
-    fprintf(file, "%s\n", writestr);
-    fclose(file);
-
-    printf("File '%s' has been created/overwritten successfully.\n", writefile);
-    sprintf(buffer,"Writing %s to %s\n",writestr,writefile);
-    syslog(LOG_DEBUG, buffer);
+    
+    ret=write(fd, writestr, strlen(writestr));
+    if (ret == -1) {       
+        syslog(LOG_ERR, "Error writing in file %s.\n", writefile);
+        close(fd); // Tentativo di chiudere comunque il file
+        return 1;
+    }  
+    close(fd);
+    syslog(LOG_DEBUG, "Writing %s to %s\n",writestr,writefile);
     closelog();
 
     return 0;
